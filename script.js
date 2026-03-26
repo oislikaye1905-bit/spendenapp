d77f14f999fb2897ceb56fcd559fa49c86c84a98
oislikaye1905-bit <o.islikaye.1905@gmail.com>
2026-03-26 08:27:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 let amount = 0;
+const goal = 5000;
 
-function updateDisplay() {
-  const display = document.getElementById("amount");
-  display.innerText = amount + " CHF";
-}
+window.onload = function () {
+  const saved = localStorage.getItem("amount");
 
-function changeAmount(value) {
-  amount = amount + value;
-
-  if (amount < 0) {
-    amount = 0;
+  if (saved !== null) {
+    amount = Number(saved);
   }
 
   updateDisplay();
+};
+
+function updateDisplay() {
+  const display = document.getElementById("amount");
+  const progressFill = document.getElementById("progressFill");
+  const percentText = document.getElementById("percentText");
+
+  display.innerText = amount + " CHF";
+
+  let percent = (amount / goal) * 100;
+  if (percent > 100) {
+    percent = 100;
+  }
+
+  progressFill.style.width = percent + "%";
+  percentText.innerText = Math.round(percent) + "%";
+
+  localStorage.setItem("amount", amount);
+}
+
+function changeAmount(value) {
+  amount += value;
+  updateDisplay();
+
+  const display = document.getElementById("amount");
+  display.style.transform = "scale(1.08)";
+
+  setTimeout(() => {
+    display.style.transform = "scale(1)";
+  }, 180);
 }
 
 function resetAmount() {
-  amount = 0;
-  updateDisplay();
-}
+  const confirmReset = confirm("Möchtest du den Betrag wirklich zurücksetzen?");
 
-// wichtig: beim Laden anzeigen
-updateDisplay();
+  if (confirmReset) {
+    amount = 0;
+    updateDisplay();
+  }
+}
